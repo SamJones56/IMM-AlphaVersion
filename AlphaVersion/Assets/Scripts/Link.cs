767bd317ae4ec5a4c63b88b6b01d52048e83c8a2
SamJones56 <b00156001@mytudublin.ie>
2023-11-20 14:47:28 +0000

    webgl fix

---
+++ AlphaVersion/Assets/Scripts/Link.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Runtime.InteropServices;
+using UnityEngine.UI;
+
+public class Link : MonoBehaviour 
+{
+
+	public void OpenRepo()
+	{
+	#if !UNITY_EDITOR
+		openWindow("https://github.com/SamJones56/IMM-AlphaVersion");
+	#endif
+    }
+
+    [DllImport("__Internal")]
+	private static extern void openWindow(string url);
+
+}
---
+++ AlphaVersion/Assets/Scripts/OpenLink.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenLink : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void OpenChannel() 
+    {
+        Application.OpenURL("https://github.com/SamJones56/IMM-AlphaVersion");
+    }  
+    
+}
